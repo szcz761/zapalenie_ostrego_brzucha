e89f8bd744a3aafa0ac71787f18daada46b58277
Szymon Czaja <sczaja@spyro-soft.com>
2019-03-19 19:16:38 +0100

    k-nearest neighbours

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import xlrd
 import os
+import numpy as np  
+import iris
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     df = pd.read_excel(os.path.join(dir_path, '../data/Stany_ostrego_brzucha-dane.xls'))
+    iris.example()
     print(df)
