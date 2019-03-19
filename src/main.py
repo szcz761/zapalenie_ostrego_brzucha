ea0ac136fd685ff3e03b7ba6e2767d2301b14b77
Szymon Czaja <sczaja@spyro-soft.com>
2019-03-19 18:20:52 +0100

    better path fix

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import xlrd
+import os
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
-    df = pd.read_excel('/home/szymon/repo/zapalenie_ostrego_brzucha/data/Stany_ostrego_brzucha-dane.xls')
-    print (df)
+    df = pd.read_excel(os.path.join(dir_path, '../data/Stany_ostrego_brzucha-dane.xls'))
+    print(df)
