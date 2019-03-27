8e3ca46a35c6e26d558704575a4ce15394be30bf
pawelsmyczek <pawel.smyczek1@gmail.com>
2019-03-27 14:20:45 +0100

    requirements file added

--- src/main.py
+++ src/main.py
@@ -8,28 +8,31 @@ from sklearn.neighbors import NearestCentroid
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def cross_validation(input):
-    split = 31
-    input = np.transpose(input)
-    input = np.random.permutation(input)
-    X,y = input[:split,:], input[split:,:]
-    y = np.ravel(y)
-    y = y.flatten()
-    print('##### X #####')
-    print(X.shape)
-    print('##### y #####')
-    print(y.shape)
+    index_end_learn = 400
+    #input = np.transpose(input)
+    #input = np.random.permutation(input)
+    lerning_data = input[:index_end_learn,:]
+    print(lerning_data)
+    test_data = input[index_end_learn:,:]
+    print(test_data)
+    # test_data = np.ravel(test_data)
+    # test_data = y.flatten()
+    print('##### lerning_data shape: #####')
+    print(lerning_data.shape)
+    print('##### test_data: #####')
+    print(test_data.shape)
     kfold = KFold(2, True, 1)
 
     #Works for y set to size of 31 not 475 yet
 
-    for train, test in kfold.split(X):
+    for train, test in kfold.split(lerning_data):
         #print('train: %s, test: %s' % (X[train], X[test]))
         train_X, test_X = X[train], X[test]
         train_y, test_y = y[train], y[test]
     return train_X, test_X, train_y, test_y
 
 if __name__ == "__main__":
-    df = pd.read_excel(os.path.join(dir_path, '../data/Stany_ostrego_brzucha-dane.xls'), header=0, skip_footer=15, parse_cols="A:AF")
+    df = pd.read_excel(os.path.join(dir_path, '../data/Stany_ostrego_brzucha-dane.xls'), header=0, skipfooter=15, usecols="A:AF")
     print(df)
     all_items = df.to_numpy()
 
