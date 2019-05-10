69cd301a5b6d90b92163d1c3bf8f1016e6928dcf
pawelsmyczek <pawel.smyczek1@gmail.com>
2019-05-10 12:00:36 +0200

    Different normalization factors added in 'def input_normalization(input):'

--- src/knn.py
+++ src/knn.py
@@ -3,68 +3,42 @@ from sklearn.neighbors import KNeighborsClassifier
 from cross_validation import cross_validation
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
+from numpy import int64
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def knn(input):    # input = np.random.permutation(input) #losowe wymieszanie wierszy
-
-    scaled_input = preprocessing.scale(input)
-    cv_scores = []
-    for i in range(1,10):
-        scores_mean = cross_validation(input, KNeighborsClassifier(n_neighbors=i, p=1, metric='minkowski'))
-        cv_scores.append(scores_mean)
-
-    Range = list(range(1,10))
-    MSE = [ x for x in cv_scores]
-    plt.figure(1)
-    plt.title("Manhattan distance measurement without normalization")
-    plt.plot(Range, MSE)
-    plt.xlabel('Number of Neighbors K')
-    plt.ylabel('Learning level')
-
-
-    cv_scores = []
-    for i in range(1,10):
-        scores_mean = cross_validation(input, KNeighborsClassifier(n_neighbors=i, p=2, metric='minkowski'))
-        cv_scores.append(scores_mean)
-
-    Range = list(range(1,10))
-    MSE = [ x for x in cv_scores]
-    plt.figure(2)
-
-    plt.title("Euclidean distance measurement without normalization")
-    plt.plot(Range, MSE)
-    plt.xlabel('Number of Neighbors K')
-    plt.ylabel('Learning level')
-
-    cv_scores = []
-    for i in range(1,10):
-        scores_mean = cross_validation(scaled_input, KNeighborsClassifier(n_neighbors=i, p=1, metric='minkowski'))
-        cv_scores.append(scores_mean)
-
-    Range = list(range(1,10))
-    MSE = [ x for x in cv_scores]
-    plt.figure(3)
-    plt.title("Manhattan distance measurement with normalization")
-    plt.plot(Range, MSE)
-    plt.xlabel('Number of Neighbors K')
-    plt.ylabel('Learning level')
-
-
-    cv_scores = []
-    for i in range(1,10):
-        scores_mean = cross_validation(scaled_input, KNeighborsClassifier(n_neighbors=i, p=2, metric='minkowski'))
-        cv_scores.append(scores_mean)
-
-    Range = list(range(1,10))
-    MSE = [ x for x in cv_scores]
-    plt.figure(4)
-
-    plt.title("Euclidean distance measurement with normalization")
-    plt.plot(Range, MSE)
-    plt.xlabel('Number of Neighbors K')
-    plt.ylabel('Learning level')
+    outputs = input_normalization(input)
+    normal_string = ["without normalization",
+                     "with [0,100] min max norm",
+                     "with mean norm",
+                     "with std norm"]
+    fig = 1
+    string_index = 0
+    for item in outputs:
+        for p in range(1,3):
+            cv_scores = []
+            for i in range(1,10):
+                scores_mean = cross_validation(item, KNeighborsClassifier(n_neighbors=i, p=p, metric='minkowski'))
+                cv_scores.append(scores_mean)
+            Range = list(range(1,10))
+            cv_scores = [x for x in cv_scores]
+            plt.figure(fig)
+            if p == 1:
+                plt.title("Manhattan distance measurement "+normal_string[string_index])
+            if p == 2:
+                plt.title("Euclidean distance measurement "+normal_string[string_index])
+            plt.plot(Range, cv_scores)
+            plt.xlabel('Number of Neighbors K')
+            plt.ylabel('Learning level')
+            fig+=1
+        string_index+=1
     plt.show()
-    print(type(scaled_input))
-    print("--------------")
-    print(type(input))
+
+def input_normalization(input): # wyskalowane dane są rzutowane do int64, póki co strata precyzji
+    no_norm = input
+    min_max = preprocessing.MinMaxScaler(feature_range=(0,100)).fit_transform(input).astype(int64)
+    mean = preprocessing.scale(input, with_mean=True).astype(int64)
+    std = preprocessing.scale(input, with_std=True).astype(int64)
+    return no_norm, min_max, mean, std
+
--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     df = pd.read_excel(os.path.join(dir_path, '../data/Stany_ostrego_brzucha-dane.xls'),
-                       header=0, skip_footer=15, parse_cols="A:AG")
-    print(df)
+                       header=0, skipfooter=15, use_cols="A:AG")
+    #print(df)
     knn(df.to_numpy())
 
     # cross_validation(all_items, svm.SVC(kernel='linear', C=1))
